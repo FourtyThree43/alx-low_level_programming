eec3cb4c6d6fbc2b1be60b70935feaf0a8196f4f
FourtyThree43 <shaqmwa@outlook.com>
2022-12-08 10:32:41 -0800

    added c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * Prints the phrase: "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly.
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
