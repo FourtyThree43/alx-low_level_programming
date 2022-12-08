248d0abbe3af67e1934fe26bd6259b6e88a5e123
FourtyThree43 <shaqmwa@outlook.com>
2022-12-08 10:53:12 -0800

    added task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: Always  0 (success)
  */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point
+ * Prints the phrase with proper grammar,but the outcome is a piece of art,
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
