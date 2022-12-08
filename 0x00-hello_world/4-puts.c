6f242475684cac96e24914d22acfb0f1017d1fc8
FourtyThree43 <shaqmwa@outlook.com>
2022-12-08 10:30:51 -0800

    added c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * Prints the phrase: "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
