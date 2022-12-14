2d6823f7e3988579fdbf2ff757e5dbd958dde8ea
FourtyThree43 <shaqmwa@outlook.com>
2022-12-14 02:17:05 -0800

    Added task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *main - Enrty point
+ *prints _putchar when ececuted
+ *Return: 0 (success)
+ **/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
