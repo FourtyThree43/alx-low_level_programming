41e0f2f9693c013dd6ba838586328f41590fa2a9
FourtyThree43 <shaqmwa@outlook.com>
2022-12-14 02:55:40 -0800

    Added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+{
+        char ch;
+
+        ch = 'a';
+
+        while (ch <= 'z')
+        {
+                _putchar(ch);
+                ch++;
+        }
+
+        _putchar('\n');
+}
