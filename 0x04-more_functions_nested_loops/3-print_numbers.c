4b1428072c01e83db6e80ec2048b1216348dce9b
FourtyThree43 <shaqmwa@outlook.com>
2022-12-15 04:32:49 -0800

    Task added

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers- prints the numbers, from 0 to 9.
- * followed by a new line.
- * Return: 0 (success)
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (int i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar('0' + i);
+		_putchar(i + '0');
 	}
+
 	_putchar('\n');
 }
