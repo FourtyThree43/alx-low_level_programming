8bde519a207bab583b3b3987fdf3d223401cc9e3
FourtyThree43 <shaqmwa@outlook.com>
2022-12-15 04:55:41 -0800

    Task added

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
- * except 2 and 4, followed by a new line.
+ * print_most_numbers - prints the numbers, from 0 to 9,
+ * except 2 and 4, followed by a new line
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
