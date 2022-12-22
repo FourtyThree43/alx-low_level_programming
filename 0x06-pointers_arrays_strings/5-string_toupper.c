d5ee3f31198710cb07a5df9db70f4b3d6c9dc6b9
FourtyThree43 <shaqmwa@outlook.com>
2022-12-22 11:54:10 -0800

    Task Added

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,12 @@ char *string_toupper(char *s)
 	int i = 0;
 
 	/* Convert each lowercase letter of the string to uppercase */
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] = s[i] - 32;
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s = *s - 'A';
+		}
 		i++;
 	}
 	/* Return a pointer to the modified string */
