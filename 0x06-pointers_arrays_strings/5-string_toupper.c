c97a8706e458f5b329452ba94f7d9833886836ca
FourtyThree43 <shaqmwa@outlook.com>
2022-12-22 11:45:41 -0800

    Task Added

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * @s: pointer to input char.
+ * Return: @s.
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	/* Convert each lowercase letter of the string to uppercase */
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+		i++;
+	}
+	/* Return a pointer to the modified string */
+	return (s);
+}
