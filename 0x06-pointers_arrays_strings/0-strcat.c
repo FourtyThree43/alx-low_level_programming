80400b34c391a0527f44dd73eedbc6b94a8a8160
FourtyThree43 <shaqmwa@outlook.com>
2022-12-22 03:52:08 -0800

    Task Added

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,11 +4,12 @@
  * _strcat - concatenates two strings.
  * and adds a terminating null byte.
  *
- * @src: String to be appended.
- * @dest: string to be appednded to.
+ * @src: pointer to source input.
+ * @dest: pointer to destination input.
  *
- * returns: char (success).
- **/
+ * Return: pointer to resulting string @dest.
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	char *ptr = dest;
@@ -19,7 +20,7 @@ char *_strcat(char *dest, char *src)
 		ptr++;
 	}
 
-/* Append the src string to the end of dest */
+/** Append the src string to the end of dest */
 	while (*src != '\0')
 	{
 		*ptr = *src;
@@ -30,6 +31,6 @@ char *_strcat(char *dest, char *src)
 /** Add a terminating null byte to the end of dest */
 	*ptr = '\0';
 
-/* Return a pointer to the resulting string dest */
+/** Return a pointer to the resulting string dest */
 	return (dest);
 }
