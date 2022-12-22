0bcfb2d293bed36411a817744c03a60905f76f52
FourtyThree43 <shaqmwa@outlook.com>
2022-12-22 13:40:33 -0800

    Task Added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+char *cap_string(char *s)
+{
+    /* Capitalize the first letter of the string */
+    if (*s >= 'a' && *s <= 'z')
+    {
+        *s = *s - 'a' + 'A';
+    }
+
+    /* Capitalize the first letter of each subsequent word */
+    while (*s != '\0')
+    {
+        if (*s == ' ' ||
+	    *s == '\t' ||
+	    *s == '\n' ||
+	    *s == ',' ||
+            *s == ';' ||
+            *s == '.' ||
+	    *s == '!' ||
+	    *s == '?' ||
+	    *s == '"' ||
+	    *s == '(' ||
+	    *s == ')' ||
+	    *s == '{' ||
+	    *s == '}')
+        {
+            s++;
+            if (*s >= 'a' && *s <= 'z')
+            {
+                *s = *s - 'a' + 'A';
+            }
+        }
+        else
+        {
+            s++;
+        }
+    }
+
+    /* Return a pointer to the modified string */
+    return s;
+}
