3df6cce23f4655efdef016fe68bc22e41d525741
FourtyThree43 <shaqmwa@outlook.com>
2023-01-03 12:22:21 +0000

    Task Added

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# ALX Low-level programming & Algorithm
+0x07. C - Even more pointers, arrays and strings
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _HEADER_FILE_H_
+#define _HEADER_FILE_H_
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif //_HEADER_FILE_H_
