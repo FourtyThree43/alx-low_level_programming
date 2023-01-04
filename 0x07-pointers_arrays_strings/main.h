60996e2dc614c75577682185d700ddc8f7460327
FourtyThree43 <shaqmwa@outlook.com>
2023-01-04 11:35:36 +0300

    Task Added

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <stddef.h>
+#include <string.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif 
