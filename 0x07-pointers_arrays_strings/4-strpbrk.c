e98e6e093f5211c90ebd35c5b544f38b849be11b
FourtyThree43 <shaqmwa@outlook.com>
2023-01-04 13:26:47 +0300

    Task Added

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _strpbrk - locates the first occurrence in the string s
+* 	     of any of the bytes in the string accept
+*
+* @s: string to be scanned.
+* @accept: string containing the characters to match.
+*
+* Return: pointer to the character in @s that matches one of the characters in @accept,
+* 	  else returns NULL.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+	/*Itarte over ther character of s */
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		{
+			
+	}
+}
