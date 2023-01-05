f55141080a1d1d663441981503016b349ef4314e
FourtyThree43 <shaqmwa@outlook.com>
2023-01-05 13:50:57 +0300

    Task Added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - calculate the length of a string.
+*
+* @s: pointer of the string.
+*
+* Return: lenth of the string.
+*/
+int _strlen_recursion(char *s)
+{
+	/*base case: if current character is nill terminator, return 0*/
+	if (*s == '\0')
+		return (0);
+
+	/*re crusive step: add 1 to the result of calling the function*/
+	/*with next character as argument*/
+	return (1 + _strlen_recursion(s + 1));
+}
