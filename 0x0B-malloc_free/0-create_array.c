823292160af014a1bbeb95f5598843beb059613f
FourtyThree43 <shaqmwa@outlook.com>
2023-01-12 22:42:06 +0300

    update - Timestamp: 1673552526

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
 /**
  * *create_array - creates an array of chars,
  * and initializes it with a specific char
- * 
+ *
  * @size: size of the array to create
  * @c: char to initialize the array c
  *
@@ -12,21 +12,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	char *p;
+	unsigned int i;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	char* array = (char*) malloc(size * sizeof(char));
-	if (array == NULL)
-	{
+	p = malloc(size * sizeof(char));
+	if (p == NULL)
 		return (NULL);
-	}
-
 	for (i = 0; i < size; i++)
-	{
-		array[i] = c;
-	}
-	return (array);
+		p[i] = c;
+	p[i] = '\0';
+	return (p);
 }
