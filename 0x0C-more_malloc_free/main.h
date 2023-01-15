4fc47b2d37b3a94d444453d7d800dfbaf06d7845
FourtyThree43 <shaqmwa@outlook.com>
2023-01-15 22:13:55 +0300

    Task Added - Timestamp: 1673810035

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,5 +13,9 @@ void free_grid(int **grid, int height);
 /*0x0C-more_malloc_free*/
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
 
 #endif
