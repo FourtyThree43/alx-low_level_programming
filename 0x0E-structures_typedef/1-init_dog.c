3a427af4853113ba8ee26d7d9eb73b3752909114
FourtyThree43 <shaqmwa@outlook.com>
2023-01-18 09:36:20 +0300

    Task Added - Timestamp: 1674023780

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -17,6 +17,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	/* (*d).owner = owner; */
 	d->owner = owner;
-	/* (*d).age = age; */
-	d->age = age;
+	(*d).age = age;
+	/*d->age = age;*/
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,5 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif /* DOG H */
+#endif /* Struct Dog */
 
