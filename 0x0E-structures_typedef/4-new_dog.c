ae43930b66f55d08b636c892b634e63e869ab655
FourtyThree43 <shaqmwa@outlook.com>
2023-01-18 20:46:51 +0300

    new dog with strdup - Timestamp: 1674064011

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,49 +1,77 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
- * new_dog - new dog
+ * *_strdup - copies the string given as parameter.
+ * From 0x0B-malloc_free/1-strdup.c
+ * @str: string to duplicate
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *dup;
+	unsigned int i, len;
+
+	i = 0;
+	len = 0;
+
+	/* Check if input string is NULL */
+	if (str == NULL)
+		return (NULL);
+
+	/* Determine the length of the input string */
+	while (str[len])
+		len++;
+
+	/* Allocate memory for the duplicate string */
+	dup = malloc(sizeof(char) * (len + 1));
+
+	/* Check if malloc failed to allocate memory */
+	if (dup == NULL)
+		return (NULL);
+
+	/* Copy the input string to the duplicate string */
+	while ((dup[i] = str[i]) != '\0')
+		i++;
+
+	/* Return the duplicate string */
+	return (dup);
+}
+
+/**
+ * new_dog - a function that creates a new dog
+ *
  * @name: name of dog
  * @age: age of dog
- * @owner: owner of the dog
+ * @owner: dog owner
  *
- * Return: a pointer with the new structure
- */
+ * Return: struct pointer dog
+ *         NULL if function fails
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	int i, n_name, n_owner;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL || !name || !owner)
+	dog_t *new_dog = malloc(sizeof(dog_t));
+	/* Return NULL if malloc failed to allocate memory */
+	if (new_dog == NULL)
+		return (NULL);
+	new_dog->name = _strdup(name);
+	/* Return NULL if strdup failed to allocate memory */
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-
-	for (n_name = 0 ; name[n_name] ; n_name++)
-		;
-	for (n_owner = 0 ; owner[n_owner] ; n_owner++)
-		;
-
-	new_dog->name = malloc(n_name + 1);
-	new_dog->owner = malloc(n_owner + 1);
-
-	if (!new_dog->name || !new_dog->owner)
+	new_dog->age = age;
+	new_dog->owner = _strdup(owner);
+	/* Return NULL if strdup failed to allocate memory */
+	if (new_dog->owner == NULL)
 	{
-		free(new_dog->owner);
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
-	for (i = 0 ; i < n_name ; i++)
-	new_dog->name[i] = name[i];
-	new_dog->name[i] = '\0';
-	new_dog->age = age;
-
-	for (i = 0 ; i < n_owner ; i++)
-	new_dog->owner[i] = owner[i];
-	new_dog->owner[i] = '\0';
+	/* Return the address of the newly created dog */
 	return (new_dog);
 }
