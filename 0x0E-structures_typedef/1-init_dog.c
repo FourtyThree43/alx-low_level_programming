c8fb2c25e06321852f9266d9091908770a259b05
FourtyThree43 <shaqmwa@outlook.com>
2023-01-18 10:44:19 +0300

    s - Timestamp: 1674027859

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,7 +13,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
-	d->name = name;
-	d->owner = owner;
-	d->age = age;
+	{
+		d->name = name;
+		d->owner = owner;
+		d->age = age;
+	}
 }
