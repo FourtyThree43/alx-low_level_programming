aa2241ab96b0cf84a40b14ad5f5caaf2688947ed
FourtyThree43 <shaqmwa@outlook.com>
2023-01-18 15:17:01 +0300

    dog_t - Timestamp: 1674044221

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,5 +19,11 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t -Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
 #endif /* Struct Dog */
 
