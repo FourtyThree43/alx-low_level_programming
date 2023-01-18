ba6bc84532ad6013f45f27297e69ca29fca3c8c7
FourtyThree43 <shaqmwa@outlook.com>
2023-01-18 14:46:27 +0300

    Another fix my Woof! - Timestamp: 1674042387

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,8 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: ");
 		d->name == NULL ? printf("nil\n") : printf("%s\n", d->name);
+		printf("Age: %f\n", d->age);
 		printf("Owner: ");
 		d->owner == NULL ? printf("nil\n") : printf("%s\n", d->owner);
-		printf("Age: %f\n", d->age);
 	}
 }
