aeb3751304443279f00e24c58e58b2d583657259
FourtyThree43 <shaqmwa@outlook.com>
2023-01-20 17:04:16 +0300

    Added colors

--- GitScript.sh
+++ GitScript.sh
@@ -1,9 +1,47 @@
 #!/bin/bash
+#
+# Copyright (C) 2023 FourtyThree43 (0x43)  <https://github.com/FourtyThree43>
+# LICENSE © GNU-GPL3
+#
+
+# a simple script to Ipush your commits to GitHub #
+
+## ------------ COLORS ------------ ##
+
+Color_Off='\033[0m' # Text Reset
+
+# Regular Colors #
+Black='\033[0;30m'  Red='\033[0;31m'     Green='\033[0;32m'  Yellow='\033[0;33m'
+Blue='\033[0;34m'   Purple='\033[0;35m'  Cyan='\033[0;36m'   White='\033[0;37m'
+
+# Bold #
+BBlack='\033[1;30m' BRed='\033[1;31m'    BGreen='\033[1;32m' BYellow='\033[1;33m'
+BBlue='\033[1;34m'  BPurple='\033[1;35m' BCyan='\033[1;36m'  BWhite='\033[1;37m'
+
+# Underline #
+UBlack='\033[4;30m' URed='\033[4;31m'    UGreen='\033[4;32m' UYellow='\033[4;33m'
+UBlue='\033[4;34m'  UPurple='\033[4;35m' UCyan='\033[4;36m'  UWhite='\033[4;37m'
+
+# Background #
+On_Black='\033[40m' On_Red='\033[41m'    On_Green='\033[42m' On_Yellow='\033[43m'
+On_Blue='\033[44m'  On_Purple='\033[45m' On_Cyan='\033[46m'  On_White='\033[47m'
+
+echo ""
+echo -e "${BCyan}#############################${Color_Off}"
+echo -e "${BCyan}#      Git Push Script      #${Color_Off}"
+echo -e "${BCyan}#############################${Color_Off}"
 
 while true; do
+  # get branch name (e.g master, main, etc... ) #
+  Branch=$(git branch --show-current) 
+
+  echo -e "\n${BRed}[*] Your Current Branch : ${BYellow}${Branch}${Color_Off}"
+
   # Get the commit message from the user
-  echo "Enter commit message:"
-  read message
+    echo ""
+    echo -e "${BPurple}##################################${Color_Off}"
+    echo -e "${BGreen}# Write your commit comment! :-  #${Color_Off}"
+    read message
 	
 	# Prompt the user for the name of the file to add
   # echo "Enter the name of the file to add:"
@@ -35,8 +73,12 @@ while true; do
   # Commit the changes with the specified message
   git commit -m "$message"
 
-  # Push the commit to the origin remote and master branch
-  git push
+  # push to repo #
+  echo ""
+  git push -u origin $Branch
+  
+  # D O N E! #
+  echo -e "\n${BGreen}[✔] D O N E \n${Color_Off}"
 
   # Exit the loop
   break
--- TaskInit.sh
+++ TaskInit.sh
@@ -1,7 +1,41 @@
 #!/bin/bash
 
+#
+# Copyright (C) 2023 FourtyThree43 (0x43)  <https://github.com/FourtyThree43>
+# LICENSE © GNU-GPL3
+#
+
+# a simple script to Initialize your ALX tasks #
+
+## ------------ COLORS ------------ ##
+
+# Reset #
+Color_Off='\033[0m' # Text Reset
+
+# Regular Colors #
+Black='\033[0;30m'  Red='\033[0;31m'     Green='\033[0;32m'  Yellow='\033[0;33m'
+Blue='\033[0;34m'   Purple='\033[0;35m'  Cyan='\033[0;36m'   White='\033[0;37m'
+
+# Bold #
+BBlack='\033[1;30m' BRed='\033[1;31m'    BGreen='\033[1;32m' BYellow='\033[1;33m'
+BBlue='\033[1;34m'  BPurple='\033[1;35m' BCyan='\033[1;36m'  BWhite='\033[1;37m'
+
+# Underline #
+UBlack='\033[4;30m' URed='\033[4;31m'    UGreen='\033[4;32m' UYellow='\033[4;33m'
+UBlue='\033[4;34m'  UPurple='\033[4;35m' UCyan='\033[4;36m'  UWhite='\033[4;37m'
+
+# Background #
+On_Black='\033[40m' On_Red='\033[41m'    On_Green='\033[42m' On_Yellow='\033[43m'
+On_Blue='\033[44m'  On_Purple='\033[45m' On_Cyan='\033[46m'  On_White='\033[47m'
+
+echo ""
+echo -e "${BPurple}####################################${Color_Off}"
+echo -e "${Red}#      Task Initialize Script      #${Color_Off}"
+echo -e "${BPurple}####################################${Color_Off}"
+
 # Prompt user for folder name
-read -p "Enter the folder name: " folder_name
+echo -e "${BYellow}Enter the folder name: ${Color_Off}"
+read folder_name
 
 # Create new folder
 mkdir "$folder_name"
@@ -17,4 +51,16 @@ parent_folder=$(basename "$(dirname "$PWD")")
 echo "# $parent_folder" > README.md
 echo "$current_folder" >> README.md
 
-echo "Task Initialized!"
+# Print the output.
+echo ""
+pwd
+
+echo ""
+ls -a
+
+echo ""
+cat README.md
+
+# D O N E! #
+echo -e "\n${BGreen}[✔] D O N E !\n${Color_Off}"
+
