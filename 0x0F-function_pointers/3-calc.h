9c4929c6a588752cbf99834f6038553588c24950
FourtyThree43 <shaqmwa@outlook.com>
2023-01-22 22:51:36 +0300

    Task Added - Timestamp: 1674417096

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,5 @@
 #ifndef _CALC_H
-##define _CALC_H
+#define _CALC_H
 
 /**
 * struct op - Struct op
@@ -13,5 +13,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
 
-#endif /*struct op*/
+#endif /*_CALC_H*/
