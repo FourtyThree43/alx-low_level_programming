ff3cd7ceee343cf2ec55bd8f31fa01c33a3fcf56
FourtyThree43 <shaqmwa@outlook.com>
2023-02-01 14:12:09 +0300

    Task Added - Timestamp: 1675249929

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * pre_main - prints a message before the main function is executed
+ *
+ * Return: void
+ */
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
