515986ccd8c2ea12c4b28f4c4c705fb307a08fb6
FourtyThree43 <shaqmwa@outlook.com>
2023-03-22 12:53:46 +0300

    syntax fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/FourtyThree43/alx-low_level_programming/raw/main/0x18-dynamic_libraries/101-libwinning.so
+wget -P /tmp https://github.com/FourtyThree43/alx-low_level_programming/raw/main/0x18-dynamic_libraries/101-libwin.so
 export LD_PRELOAD=/tmp/101-libwin.so
