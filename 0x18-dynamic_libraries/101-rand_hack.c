d6f1ad299e2209bb081779a1dd82fd56148db6be
FourtyThree43 <shaqmwa@outlook.com>
2023-03-22 12:50:47 +0300

    Renamed files, added rand hack.

---
+++ 0x18-dynamic_libraries/101-rand_hack.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <dlfcn.h>
+
+/* function pointer to the original rand & srand function */
+int (*original_rand)(void);
+int (*original_srand)(unsigned int seed);
+
+/* get the pointer to the original rand & srand function */
+void __attribute__((constructor)) init(void)
+{
+	printf("init function called\n");
+	original_rand = dlsym(RTLD_NEXT, "rand");
+	original_srand = dlsym(RTLD_NEXT, "srand");
+}
+
+void srand(unsigned int seed)
+{
+	printf("Intercepted srand with seed %u\n", seed);
+	original_srand(seed);
+}
+
+int rand(void)
+{
+	int value = original_rand();
+
+	static const int winning_numbers[] = {9, 8, 10, 24, 75, 10};
+	static int next_index = 0;
+	int number = winning_numbers[next_index];
+	next_index = (next_index + 1) % 6;
+
+	value = number - 1;
+	printf("Intercepted rand with value %d\n", value);
+
+	return value;
+}
